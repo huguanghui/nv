0fbb3b6c3672edd8fe509bbcaf34e5774dbdb95b
huguanghui <huguanghui1971@gmail.com>
2026-01-14 17:39:54 +0800

    fix: 优化配置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
+local term = require("snacks.terminal")
+local lazygit = require("snacks.lazygit")
+local opts = { noremap = true, silent = true }
+
+-- escape
+map("i", "jk", "<ESC>:w<CR>", opts)
+
+-- buffers
+map("n", "X", ":bdelete!<CR>", opts)
+map("n", "L", ":BufferLineCycleNext<CR>", opts)
+map("n", "H", ":BufferLineCyclePrev<CR>", opts)
+map("n", "gl", vim.diagnostic.open_float, opts)
+map("n", ";p", '"0p', opts)
+map("n", ";c", '"_c', opts)
+map("n", ";d", '"_d', opts)
+
+require("which-key").add({
+  { "<leader>a", group = "Avante", icon = " " },
+})
